7d8a9770b71ba56273965cbcfa74d4d5febe9e15
yuri-alisson <92646261+yuri-alisson@users.noreply.github.com>
2022-01-16 16:15:04 -0300

    Add files via upload

---
+++ Python/1° Aula.py
@@ -0,0 +1,19 @@
+#comentário
+print("Comentário")
+
+#Colocar dados do usuário
+nome = input("Digite seu nome: ")
+
+print(nome)
+
+#Observação: Toda variável alimentada por input é uma STRING. 
+
+#Interações com Python
+ano = input("Digite seu ano de nascimento: ")
+#conversão de variável
+idade = 2022-int(ano)
+
+print(idade)
+
+#exercicio 01
+
---
+++ Python/Exercicios.py
@@ -0,0 +1,26 @@
+# #exercicio 01
+
+# nota1 = input ("Nota 01 ")
+# nota2 = input ("Nota 02 ")
+# nota3 = input ("Nota 03 ")
+
+# media = (int(nota1)+int(nota2)+int(nota3))/3
+
+# print(media)
+
+#exercicio 02
+
+# nome =input("Digite o nome ")
+# sobrenome =input("Digite o sobrenome ")
+# idade =input("Digite a idade ")
+
+# print(nome+" "+sobrenome+" "+idade)
+
+#exercicio 03
+
+rua =input("Digite o rua ")
+bairro =input("Digite o bairro ")
+cep =input("Digite a cep ")
+ccidade =input("Digite a cidade ")
+
+print(rua)
